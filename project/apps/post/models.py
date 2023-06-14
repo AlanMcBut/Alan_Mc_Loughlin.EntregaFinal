f82828837759ec6fb503b1be35b25757b7f158d0
AlanMcBut <alanmcbut@gmail.com>
2023-06-14 12:50:34 -0300

    Terminados los modelos

--- project/apps/post/models.py
+++ project/apps/post/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 class Post(models.Model):
-    titulo = models.CharField(max_length=50)
+    titulo = models.CharField(max_length=50, blank=False)
     juego = models.ForeignKey('post.NombreJuego', null=False, on_delete=models.CASCADE)  
     rango = models.CharField(max_length=15, null=True)
-    contenido = models.TextField
-    fecha_de_publicacion = models.DateTimeField
+    cuenta_del_juego = models.CharField(max_length=50, blank=False, default='Ingrese su usuario del juego')
+    contenido = models.TextField(max_length=200, default='Detalles sobre su peticion')
+    fecha_de_publicacion = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
         return self.titulo
