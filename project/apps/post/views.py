9396abf1ca39e7f1eda241fb4bbc8349e15f8458
AlanMcBut <alanmcbut@gmail.com>
2023-06-15 20:55:19 -0300

    Creado el nav bar

--- project/apps/post/views.py
+++ project/apps/post/views.py
@@ -5,11 +5,12 @@ from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     return render (request, "post/index.html")
 
-class AnuncioCrear(CreateView):
+class AnuncioCrear(LoginRequiredMixin, CreateView):
     model = models.Post
     form_class = forms.PostForm
     template_name = "post/crear-anuncio.html"
@@ -20,7 +21,7 @@ class AnuncioLista(ListView):
     template_name = "post/lista-anuncios.html"
     context_object_name = "lista_anuncios"
 
-class AnuncioDetalle(DetailView):
+class AnuncioDetalle(LoginRequiredMixin, DetailView):
     model = models.Post  
     template_name = "post/detalle-anuncio.html"  
 
